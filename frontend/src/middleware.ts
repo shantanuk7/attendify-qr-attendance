ea00c3b0eabbda5136295581b86379ce1a0699ce
Atharva0506 <atharvan.coder@gmail.com>
2024-12-09 21:50:46 +0530

    middleware

---
+++ frontend/src/middleware.ts
@@ -0,0 +1,32 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import {jwtDecode} from "jwt-decode";
+
+export function middleware(request: NextRequest) {
+  const token = request.cookies.get("token")?.value;
+
+  if (!token) {
+    return NextResponse.redirect(new URL("/auth/signin", request.url));
+  }
+
+  try {
+    const { role }: { role: string } = jwtDecode(token);
+    const pathname = request.nextUrl.pathname;
+
+    if (role === "admin" && !pathname.startsWith("/admin")) {
+      return NextResponse.redirect(new URL("/admin", request.url));
+    }
+
+    if (role === "user" && !pathname.startsWith("/user")) {
+      return NextResponse.redirect(new URL("/user", request.url));
+    }
+  } catch {
+    return NextResponse.redirect(new URL("/auth/singup", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/admin/:path*", "/user/:path*"],
+};
