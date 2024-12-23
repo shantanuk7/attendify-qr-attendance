f85b64cbafd8e9171cad5993b0851423a60cf7bf
Atharva0506 <atharvan.coder@gmail.com>
2024-12-23 16:00:00 +0530

    removing qr from backend no need

--- backend/src/utils/generateQR.ts
+++
@@ -1,9 +0,0 @@
-import QRCode from 'qrcode';
-
-export const generateQRCode = async (data: string): Promise<string> => {
-    try {
-        return await QRCode.toDataURL(data);
-    } catch (err) {
-        throw new Error('Error generating QR Code');
-    }
-};
